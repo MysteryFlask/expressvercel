b383365f676621fb1df64443009a78d12a6139fd
MysteryFlask <mazzua24@gmail.com>
2023-12-27 16:08:19 +0000

    Minor fixes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const product = require("./api/scrape");
+const scrape = require("./api/scrape");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/scrape", product);
+app.use("/api/scrape", scrape);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
